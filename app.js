6ca08fc6dd1130c65a47833dc926a16932ca3efd
Pranav Jagtap <79315244+PranavJ10-srt@users.noreply.github.com>
2022-01-16 14:17:12 +0530

    Fetch first 30 records in descending

--- app.js
+++ app.js
@@ -15,9 +15,43 @@ MongoClient.connect(url, function(err, client)
 {
 
    var db = client.db('__CONCOX__');
-    var cursor = db.collection('devices').find();
+    var cursor_device = db.collection('devices').find();
      // console.log(cursor.devices._id);
-    cursor.forEach(console.log);
+     var id=[];
+     cursor_device.sort({createdAt:-1});
+     let cnt=30;
+    cursor_device.forEach(function(item){
+      if(cnt>0){
+        id.push(item.id);
+       console.log(item);
+     }
+
+      cnt--;
+    });
+    console.log(id);
+    //var cursor_status = db.collection('status').find();
+
 
 }
 )
+
+
+/*const uri = "mongodb+srv://backendconcoxdeveloper:V3jUV7QXqEoAtnhy@cluster0-zhjde.mongodb.net"
+MongoClient.connect(uri , function(err,client)
+{
+  var db = client.db('__CONCOX__');
+  var cursor = db.collection('devices').find();
+  cursor.forEach(console.dir);
+})*/
+app.get("/" , function(req,res){
+  Status.find(function(err,results){
+    if(err)
+    console.log(err);
+    else
+    console.log(results);
+  });
+  // console.log((mongoose.find());
+});
+app.listen(3000,function(){
+  console.log("server started");
+});
